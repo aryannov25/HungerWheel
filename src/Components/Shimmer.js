65e29420f53bb981014fd53bcb0520807f6b5df2
Aryan Chaudhary <aryannov25@gmail.com>
2023-02-13 14:43:11 +0530

    update

--- src/Components/Shimmer.js
+++ src/Components/Shimmer.js
@@ -1,10 +1,13 @@
 const Shimmer = () => {
   return (
-    <div className="restaurant-list " >
+    <div className="restaurant-list ">
       {Array(10)
         .fill("")
         .map((e, index) => (
-          <div key={index} className="shimmer-card flex flex-wrap w-60 p-2 mt-2 m-2 h-full bg-purple-50"></div>
+          <div
+            key={index}
+            className="shimmer-card flex flex-wrap w-60 p-2 mt-2 m-2 h-full bg-purple-50"
+          ></div>
         ))}
     </div>
   );
