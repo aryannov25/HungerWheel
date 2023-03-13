3788f3b94b2eab08927d44d8b1db15913c0a16b6
Aryan Chaudhary <aryannov25@gmail.com>
2023-03-13 20:35:33 +0530

    update

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -1,18 +1,13 @@
 export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
-
-
-  
-
 export function filterData(searchText, restaurants) {
   const filterData = restaurants.filter((restaurant) =>
-    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
+    restaurant?.cuisines?.toLowerCase()?.includes(searchText.toLowerCase())
   );
 
   return filterData;
 }
-
 
 export const FETCH_MENU_URL =
   "https://www.swiggy.com/dapi/menu/v4/full?lat=28.5270362&lng=77.13593279999999&menuId=";
