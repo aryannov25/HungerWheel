35ce0ff6753f555034df559e52c608cfa531812b
Aryan Chaudhary <aryannov25@gmail.com>
2023-03-17 12:09:59 +0530

    update

--- src/Components/cartSlice.js
+++ src/Components/cartSlice.js
@@ -1,8 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-
-   name: "cart",
-   i
-
-})
+  name: "cart",
+  initialState: {
+    items: ["Banana", "Apples"],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+    removeItem: (state, action) => {
+      state.items.pop();
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
