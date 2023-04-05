dece8cd1e3a15eadb33c69a58f21dc762fbec61c
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-05 19:13:19 +0530

    update

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -10,7 +10,7 @@ export function filterData(searchText, restaurants) {
 }
 
 export const FETCH_MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5270362&lng=77.13593279999999&restaurantId=";
+  "https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5270362&lng=77.13593279999999&restaurantId=";
 
 // lat=28.5270362&lng=77.13593279999999
 
