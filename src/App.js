0c78dda3c37cd5132d23f9c3559d4c384a0fc93c
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-18 00:19:11 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import Restaurant from "./Components/Restaurant";
 import Nav from "./Components/Navbar";
 import Footer from "./Components/Footer";
 
-
-
 function App() {
   return (
     <React.StrictMode>
-      <Nav/>
+      <Nav />
       <Restaurant />
       <Footer />
     </React.StrictMode>
