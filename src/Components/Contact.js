73421b8d5db6782281620d83027eb5b77cdb8f95
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-20 14:13:04 +0530

    update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,12 +1,183 @@
-import React from "react";
+import { React, useRef } from "react";
 import Nav from "./Navbar.js";
 import Footer from "./Footer";
+import { useState, useEffect } from "react";
 
 function Contact() {
+  const [message, setMessage] = useState({});
+  const [email, setEmail] = useState({});
+  const [name, setName] = useState({});
+  const messageInputRef = useRef();
+  const emailInputRef = useRef();
+  const nameInputRef = useRef();
+
+
+  function submitHandler(e) {
+    e.preventDefault();
+    e.target.reset();
+
+    const setMessage = messageInputRef.current.value;
+    const setEmail = emailInputRef.current.value;
+    const setName = nameInputRef.current.value;
+
+    const Data = {
+      message: setMessage,
+      email: setEmail,
+      name: setName,
+    };
+
+    //Post Tweets
+    fetch(
+      "https://fir-auth-5ce51-default-rtdb.firebaseio.com/hungerwheel.json",
+      {
+        method: "POST",
+        body: JSON.stringify(Data),
+      },
+      alert("success")
+    );
+  }
+
   return (
     <>
       <Nav />
-      <div>Contact Page</div>
+
+      <div>
+        <form
+          className="text-gray-700 body-font relative"
+          onSubmit={submitHandler}
+        >
+          <div className="container px-5 py-16 mx-auto">
+            <div className="flex flex-col text-center w-full mb-12">
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                Contact Us
+              </h1>
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+                gentrify.
+              </p>
+            </div>
+            <div className="lg:w-1/2 md:w-2/3 mx-auto">
+              <div className="flex flex-wrap -m-2">
+                <div className="p-2 w-1/2">
+                  <div className="relative">
+                    <label
+                      for="name"
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      Name
+                    </label>
+                    <input
+                      ref={nameInputRef}
+                      type="text"
+                      id="name"
+                      name="name"
+                      className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                </div>
+                <div className="p-2 w-1/2">
+                  <div className="relative">
+                    <label
+                      for="email"
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      Email
+                    </label>
+                    <input
+                      ref={emailInputRef}
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                </div>
+                <div className="p-2 w-full">
+                  <div className="relative">
+                    <label
+                      for="message"
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      ref={messageInputRef}
+                      id="message"
+                      name="message"
+                      className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="p-2 w-full">
+                  <button
+                    type="submit"
+                    className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                  >
+                    Button
+                  </button>
+                </div>
+                <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                  <a className="text-indigo-500">example@email.com</a>
+                  <p className="leading-normal my-5">
+                    49 Smith St.
+                    <br />
+                    Saint Cloud, MN 56301
+                  </p>
+                  <span className="inline-flex">
+                    <a className="text-gray-500">
+                      <svg
+                        fill="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                      </svg>
+                    </a>
+                    <a className="ml-4 text-gray-500">
+                      <svg
+                        fill="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                      </svg>
+                    </a>
+                    <a className="ml-4 text-gray-500">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <rect
+                          width="20"
+                          height="20"
+                          x="2"
+                          y="2"
+                          rx="5"
+                          ry="5"
+                        ></rect>
+                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                      </svg>
+                    </a>
+                    
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+
       <Footer />
     </>
   );
