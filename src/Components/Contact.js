7965447c14fda72aaad5139ea138b01fc94bed61
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-21 20:59:13 +0530

    update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -115,7 +115,7 @@ function Contact() {
                     type="submit"
                     className="flex mx-auto text-white bg-orange-500 border-0 py-2 px-6 focus:outline-none hover:bg-orange-600 rounded-lg text-lg"
                   >
-                    Button
+                    Submit
                   </button>
                 </div>
               </div>
