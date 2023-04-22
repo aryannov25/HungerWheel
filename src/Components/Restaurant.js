9084e3ac7bf557dd917fa658ec3e2980eb0dd3c0
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-23 00:27:33 +0530

    update

--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -47,7 +47,7 @@ function Restaurant() {
 
   return (
     <>
-    <Nav />
+      <Nav />
       <div className="search-container p-3 text-center w-full bg-orange-50 ">
         <input
           data-testid="search-input"
