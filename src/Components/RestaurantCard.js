1e4455312a891b9133040a89ff98024abc647728
Aryan Chaudhary <aryannov25@gmail.com>
2023-07-28 21:39:43 +0530

    update

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -14,7 +14,7 @@ const RestaurantCard = ({
     <div className="card w-56 p-2 mt-2 m-4 h-96 bg-purple-50 transition duration-500 ease-in-out hover:scale-105 hover:shadow-slate-400">
       <img src={IMG_CDN_URL + cloudinaryImageId} />
       <h3 className="font-bold text-lg">{name}</h3>
-      <h5>{cuisines.join(", ")}</h5>
+      <h5>{cuisines.join(", ").slice(0, 30)}</h5>
       <h3>{areaName}</h3>
       <span>
         <h4> &#9733; {avgRating}</h4>
