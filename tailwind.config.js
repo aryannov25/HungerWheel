e6ea088d3ca1e17d109528ef21bf9c6509d8d3d4
Aryan Chaudhary <aryannov25@gmail.com>
2024-02-04 00:41:36 +0530

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shimmer: "shimmer 2s infinite linear",
+      },
+      backgroundImage: (theme) => ({
+        shimmer:
+          "linear-gradient(to right, #f8f8f8 40%, #ededed 50%, #f8f8f8 60%)",
+      }),
+      backgroundSize: {
+        "200%": "200%",
+      },
+    },
   },
   plugins: [],
 };
